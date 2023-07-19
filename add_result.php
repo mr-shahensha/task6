d95138970d8cf580445c406cb94f3305fdd63d48
Shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-07-19 15:15:50 +0530

    dynamic menu
    dynamic menu

--- add_result.php
+++ add_result.php
@@ -1,6 +1,12 @@
 <?php
 include("Connection.php");
+if(isset($_POST['submit'])){
 $root=$_REQUEST['root'];
 $menu=$_REQUEST['menu'];
 $query=mysqli_query($con,"INSERT INTO `menu` (`sl`, `root`, `menu`) VALUES (NULL, '$root', '$menu');");
+}
 ?>
+<script>
+    alert("done")
+    document.location="index.php";
+</script>
--- index.php
+++ index.php
@@ -8,49 +8,150 @@ include("connection.php");
     <title>task6</title>
 </head>
 <body>
+    <form action="add_result.php" method="post">
     <table border="2">
     <tr>
         <td>root : </td>
         <td>
-            <select name="" id="root">
+            <select name="root" id="root">
+            <option value="">select root</option>
             <?php
             $query=mysqli_query($con,"SELECT * FROM `menu`");
-            $count=mysqli_num_rows($query);
-            if($count==0)
-            {
-            ?>
-            <option value="0">root</option>
-            <?php
-            }else{
-            ?>
-            <option value="0">root</option>
-            <?php 
             while($result=mysqli_fetch_assoc($query)){
                 $sl=$result['sl'];
-                $root=$result['root'];
+                $root0=$result['root'];
                 $menu=$result['menu'];
                 ?>
-                <option value="<?php echo $root;?>"><?php echo $menu;?></option>
+                <option value="<?php echo $sl;?>">
                 <?php
+                if($root0==0){
+                    echo "main ".$menu;
+                }else{
+                    $call_menu=mysqli_query($con,"select * from menu where sl='$root0'");
+                    while($r=mysqli_fetch_assoc($call_menu)){
+                        $m=$r['menu'];
+                        $rr=$r['sl'];
+                echo $m."-".$rr."-".$menu;
+                }
             }
+               ?>
+               </option>
+                <?php
             }
             ?>
             </select>
         </td>
-        <td>next menu</td>
-        <td><input type="text" id="menu"></td>
-        <td><input type="button" value="submit" onclick="add()"></td>
+        <td> menu</td>
+        <td><input type="text" name="menu" id="menu"></td>
+        <td><input type="submit" name="submit" value="submit"></td>
     </tr>
     </table>
-    <br>
-    <div id="result"></div>
+    </form>
+            <br>
+    <table border="2">
+            <tr>
+                <td>menu -> </td>
+                <?php
+                $query=mysqli_query($con,"select * from menu where root='1'");
+                while($result=mysqli_fetch_assoc($query)){
+                    $menu=$result['menu'];
+                    $root=$result['root'];
+                    $sl=$result['sl'];
+                    ?>
+                    <td><select name="" id="">
+                        <option value="<?php echo $root;?>"><?php echo $menu;?></option>
+                        <?php
+                        $query1=mysqli_query($con,"select * from menu where root='$sl'");
+                        while($result1=mysqli_fetch_assoc($query1)){
+                            $menu1=$result1['menu'];
+                            $root1=$result1['root'];
+                                ?>
+                        <option value="<?php echo $root1;?>" ><?php echo $menu1;?></option>
+                                <?php
+                        }
+                        ?>
+                    </select>
+                    </td>
+                    <?php
+                }
+                ?>
+            </tr>
+    </table>
+<br>
+
+
+<table border="1">
+<tr>
+            <?php
+                $query=mysqli_query($con,"select * from menu where root='1'");
+                while($result=mysqli_fetch_assoc($query)){
+                    $menu=$result['menu'];
+                    $root=$result['root'];
+                    $sl=$result['sl'];
+                    
+                    // $count_c_p=mysqli_num_rows(mysqli_query($con,"select * from menu where root='$sl'"));
+                    ?>
+                    <td><?php  echo $menu;?></td>
+
+                    <?php
+                }
+                    ?>
+</tr>
+
+<tr>
+                    <?php
+                  $qry=mysqli_query($con,"select * from menu where root='1'");
+                  while($rrr=mysqli_fetch_assoc($qry)){     
+                    $sl3=$rrr['sl'];
+                    ?>
+                        <td >
+                        <table border="1">
+                            <?php
+                            $sl4="";
+                                $query2=mysqli_query($con,"select * from menu where root='$sl3'");
+                                while($result2=mysqli_fetch_assoc($query2)){
+                                    $sl4=$result2['sl'];
+                                    $menu2=$result2['menu'];
+                                    $root2=$result2['root'];
+                                    ?>
+                                        <tr>
+                                            <td>
+                                                <table border="1">
+                                                    <tr>
+                                                        <td>
+                                                        <?php echo $menu2;?>
+                                                        </td>
+                                                    </tr>
+                                                    <?php
+                                                   
+                                                    $query3=mysqli_query($con,"select * from menu where root='$sl4'");
+                                                    while($result3=mysqli_fetch_assoc($query3)){
+                                                        $menu3=$result3['menu'];
+                                                        $root3=$result3['root'];
+                                                    
+                                                        ?>
+                                                        <tr>
+                                                            <td>
+                                                                <?php  echo $menu3;?>
+                                                            </td>
+                                                        </tr>
+                                                <?php 
+                                                    }
+                                                ?> 
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                 }
+                                ?>
+                                
+                                </table>
+                        </td>
+                      <?php
+                    }
+                    ?>
+</tr>
+</table>
+
 </body>
 </html>
-
-<script>
-    function add(){
-    var root=document.getElementById('root').value;
-    var menu=document.getElementById('menu').value;
-        $('#result').load('add_result.php?root='+root+'&menu='+menu).fadeIn('fast');
-    }
-</script>
