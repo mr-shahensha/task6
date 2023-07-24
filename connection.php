5e44896dfee042b1c603abd60b1340cfd830ed1a
Shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-07-24 12:14:38 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $server="localhost";
 $username="root";
 $password="";
-$database="task6";
+$database="task";
 if(!$con=mysqli_connect($server,$username,$password,$database)){
     echo "failed to connect";
 }
